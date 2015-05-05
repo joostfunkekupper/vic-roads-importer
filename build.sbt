64cd0929f185c7f939348c893ade620a0b66a50d
Joost Funke Kupper <joostfunkekupper@gmail.com>
2015-05-05 15:54:22 +1000

    ENHANCE: Import data into Postgres

--- build.sbt
+++ build.sbt
@@ -3,3 +3,12 @@ name := "VicRoadsImporter"
 version := "0.1.0"
 
 scalaVersion := "2.11.6"
+
+libraryDependencies ++= Seq (
+	"org.postgresql"  %  "postgresql" 			 % "9.4-1200-jdbc41",
+  "org.scalikejdbc" %% "scalikejdbc"       % "2.2.6",
+  "com.h2database"  %  "h2"                % "1.4.187",
+  "ch.qos.logback"  %  "logback-classic"   % "1.1.3",
+  "org.apache.spark" %% "spark-core" 			 % "1.2.0",
+  "org.apache.spark" %% "spark-sql" 			 % "1.2.0"
+)
---
+++ src/main/scala/VicroadsImporter.scala
@@ -0,0 +1,74 @@
+package VicroadsImporter
+
+import scalikejdbc._
+import org.apache.spark._
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.joda.time._
+import org.joda.time.format._
+
+object VicroadsImporter {
+
+    def fetchSitesIds(): List[(Int, Int)] = {
+        DB readOnly { implicit session =>
+            sql"SELECT id, number FROM sites WHERE type = 'scats'".map(rs => (rs.int("id"), rs.int("number"))).list.apply()
+        }
+    }
+
+    def populateSiteAlarms(siteAlarms: Array[SiteAlarmRecord]) {
+        DB localTx { implicit session => 
+            siteAlarms.foreach { siteAlarm =>
+                sql"INSERT INTO site_alarms (site_id, timestamp, event, start) VALUES (${siteAlarm.site_id}, ${siteAlarm.timestamp}, ${siteAlarm.event}, ${siteAlarm.start});".update.apply()
+            }
+        }
+    } 
+
+    case class SiteAlarmRecord (
+        site_id     : Int,
+        timestamp   : DateTime,
+        event       : String,
+        start       : Boolean
+    )
+
+    def main(args: Array[String]) {
+
+        val conf = new SparkConf().setMaster("local").setAppName("VicroadsImporter") 
+        val sc = new SparkContext(conf)
+
+        Class.forName("org.postgresql.Driver")
+        ConnectionPool.singleton(
+            "jdbc:postgresql://localhost:5432/vicroads_ahi",
+            "postgres", "postgres"
+        )
+
+        // Query the database for scats in the sites table
+        val scatsSites = fetchSitesIds()
+        val (scat_ids, scat_numbers) = scatsSites.unzip
+
+        // TODO: Convert to use arguments
+        val csvFile = sc.textFile("alarms.csv")
+
+        val header = csvFile.map(line => line.split(",").map(_.trim)).first
+
+        val data = csvFile.map(line => line.split(",")
+                          .map(_.trim))
+                          .filter(_(0) != header(0))
+
+        val siteAlarms = data
+            .filter(sa => sa(5) != "" && scat_numbers.contains(sa(5).toInt) && (sa(6) == "+" || sa(6) == "-") && sa(7).length > 0)
+            .map(sa => SiteAlarmRecord(
+                scatsSites.filter( {case (k, v) => (v == sa(5).toInt) }).map(_._1).head,
+                DateTime.parse("%s %s".format(sa(2), sa(3)), DateTimeFormat.forPattern("dMMMYY HH:mm:ss")),
+                sa(7),
+                (sa(6) == "+"))
+            ).collect().distinct
+
+        println("\nNumber of site valid site alarms: %s\n".format(siteAlarms.length))
+
+        // Insert alarm in site alarm
+        populateSiteAlarms(siteAlarms)
+
+        println("\n------ End of program ------\n")
+    }
+}
